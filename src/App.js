6784f402a4d8f7b4f08fa2d2302ccc1971d7a76b
Andreas Abaidoo <abaidooandy@gmail.com>
2022-09-27 10:12:02 +0000

    Inital Setup and Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react';
+// import logo from './react-logo.png';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Page = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src={logo} alt="react-logo" width="125"/>
+                </nav>
+            </header>
+            <h1>Reasons why I'm excited to learn React</h1>
+            <ol>
+                <li>React cool and popular</li>
+                <li>React is powerful</li>
+                <li>React has great community support</li>
+                <li>React is maintained by facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+            </ol>
+
+            <footer>
+                © 2022 Abaidoo development. All rights reserved.
+            </footer>
+        </>
+    )
+}
+
+
+const App = () => {
+    return (
+        <div>
+            <Page />
+        </div>
+    )
 }
 
 export default App;
